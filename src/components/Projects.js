39e499d62e366be0b6353dc8da6ebf22a784caf3
Uzias Rivera <uziasrivera@gmail.com>
2020-11-05 23:39:36 -0800

    scaffolding

---
+++ src/components/Projects.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Projects = () => {
+    return (
+        <div className="projects">
+            <div className="projectTextWrap">
+                <p className="projectText">PROJECTS</p>
+            </div>
+            <div>
+
+            </div>
+        </div>
+    );
+};
+
+export default Projects;
